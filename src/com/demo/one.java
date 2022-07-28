64bf063722bf55296b7a8b556b5ce00fa1ae59b3
vardhinireddy <vardhini630@gmail.com>
2022-07-28 13:59:47 +0530

    FirstCommit

---
+++ src/com/demo/one.java
@@ -0,0 +1,26 @@
+package com.demo;
+import java.util.*;
+
+public class one {
+
+	public static void main(String[] args) {
+		int n=5;
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++)
+			{
+				if(j==0||j==4||i==2) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				
+				}
+				System.out.println();
+				
+				
+	
+	}
+}
+	}
+}
+		
